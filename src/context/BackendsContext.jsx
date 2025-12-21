cc55d6b7dd1c652baba738f80bcc422887727adc
Hector Javier Moreno <hectorjaviermorenoh@gmail.com>
2025-12-21 10:38:15 -0500

    prueba github

--- src/context/BackendsContext.jsx
+++ src/context/BackendsContext.jsx
@@ -53,6 +53,7 @@ export function BackendsProvider({ children }) {
     const newBackend = {
       id: crypto.randomUUID(),
       alias: alias.trim(),
+      // url: url.trim().charAt(0).toLowerCase() + url.trim().slice(1),
       url: url.trim().charAt(0).toLowerCase() + url.trim().slice(1),
       avatar,
     };
