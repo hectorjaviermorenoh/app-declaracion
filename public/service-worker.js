f21ad45ef92d247678cb98a52e5944443a11daad
Hector Javier Moreno <hectorjaviermorenoh@gmail.com>
2026-01-13 12:51:47 -0500

    120126 adicion Skeleton funcionando OK

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1201260144pm"; 
+const CACHE_VERSION = "v1301261237pm"; 
 const CACHE_NAME = `app-declaracion-${CACHE_VERSION}`;
 
 /* Archivos básicos que queremos disponibles */
