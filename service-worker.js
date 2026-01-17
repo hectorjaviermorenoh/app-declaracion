3af87c3a70c77cbf424f4dcd69992b79cce1d15f
Hector Javier Moreno <hectorjaviermorenoh@gmail.com>
2026-01-17 13:17:13 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1701261001am"; 
+const CACHE_VERSION = "v1701260116pm"; 
 const CACHE_NAME = `app-declaracion-${CACHE_VERSION}`;
 
 /* Archivos básicos que queremos disponibles */
