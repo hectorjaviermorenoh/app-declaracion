b58070b3ec143cd63d0dea8d261be80d97f8fae6
Hector Javier Moreno <hectorjaviermorenoh@gmail.com>
2026-02-12 13:11:38 -0500

    1242pm CORRECION CACHE GH-PAGES package.json service-worker.js main.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -160,7 +160,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className="text-decoration-none text-secondary mx-2"
           >
-            Términos hoy 1241pm
+            Términos hoy 1311pm
           </a>
         </div>
       </div>
