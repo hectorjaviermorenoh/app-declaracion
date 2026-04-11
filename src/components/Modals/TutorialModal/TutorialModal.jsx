02428d2bd404a0f7896c884a92a3128f0ea76ee8
Hector Javier Moreno <hectorjaviermorenoh@gmail.com>
2026-04-10 22:25:03 -0500

    100426  cambio en TutorialModal.jsx y scss

--- src/components/Modals/TutorialModal/TutorialModal.jsx
+++ src/components/Modals/TutorialModal/TutorialModal.jsx
@@ -6,24 +6,24 @@ const TutorialModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="tutorial-modal-overlay" onClick={onClose}>
+
       <div className="tutorial-modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="close-button" onClick={onClose} aria-label="Cerrar tutorial">
+        {/* Botón X de cerrar (flotante) */}
+        <button className="close-x-btn" onClick={onClose} aria-label="Cerrar">
           &times;
         </button>
 
-        <div className="video-container">
-          <iframe
-            src="https://player.vimeo.com/video/1177499805?h=88dc37414d&badge=0&autopause=0&autoplay=1"
-            frameBorder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowFullScreen
-            title="Video Tutorial AppDeclaración"
-          ></iframe>
+        <div className="modal-body">
+          <div className="video-aspect-ratio">
+            <iframe
+              src="https://player.vimeo.com/video/1177499805?h=88dc37414d&badge=0&autopause=0&autoplay=1"
+              allow="autoplay; fullscreen; picture-in-picture"
+              allowFullScreen
+              title="Video Tutorial AppDeclaración"
+            ></iframe>
+          </div>
         </div>
 
-        <div className="modal-footer-text">
-          <p>Descubre cómo gestionar tus documentos con AppDeclaración</p>
-        </div>
       </div>
     </div>
   );
