d83017bd8b4107381f8af28080a3c0e2e4b88301
Hector Javier Moreno <hectorjaviermorenoh@gmail.com>
2026-04-15 00:07:51 -0500

    150426 cambio video como utilizar la aplicación

--- src/components/Modals/TutorialModal/TutorialModal.jsx
+++ src/components/Modals/TutorialModal/TutorialModal.jsx
@@ -16,7 +16,9 @@ const TutorialModal = ({ isOpen, onClose }) => {
         <div className="modal-body">
           <div className="video-aspect-ratio">
             <iframe
-              src="https://player.vimeo.com/video/1177499805?h=88dc37414d&badge=0&autopause=0&autoplay=1"
+              // src="https://player.vimeo.com/video/1177499805?h=88dc37414d&badge=0&autopause=0&autoplay=1"
+              // src="https://player.vimeo.com/video/1183232192?h=081ec7be4b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+              src="https://player.vimeo.com/video/1183232192?h=081ec7be4b&badge=0&autopause=0&autoplay=1"
               allow="autoplay; fullscreen; picture-in-picture"
               allowFullScreen
               title="Video Tutorial AppDeclaración"
